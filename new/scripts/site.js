333a3c3deff8fc199c81f882b903b30628a3c173
Shota Shimazu <hornet.live.mf@gmail.com>
2017-11-29 01:46:13 +0900

    add toast plugin

---
+++ new/scripts/site.js
@@ -0,0 +1,23 @@
+$.toast({ 
+    text : "Welcome to SHOTA portfolio site.", 
+    showHideTransition : 'slide',
+    bgColor : '#4FC1E9',
+    textColor : '#eee',
+    allowToastClose : false,
+    hideAfter : 5000,
+    stack : 5,
+    textAlign : 'left',
+    position : 'bottom-left'
+});
+
+$.toast({ 
+    text : "This web site is now under construction.", 
+    showHideTransition : 'slide',
+    bgColor : '#4FC1E9',
+    textColor : '#eee',
+    allowToastClose : false,
+    hideAfter : 5000,
+    stack : 10,
+    textAlign : 'left',
+    position : 'bottom-left'
+});
