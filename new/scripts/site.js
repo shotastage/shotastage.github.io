889f74f514594982b38e730564a22754ec386717
Shota Shimazu <hornet.live.mf@gmail.com>
2017-11-29 16:22:13 +0900

    fix hide after time

--- new/scripts/site.js
+++ new/scripts/site.js
@@ -6,6 +6,7 @@ $.toast({
     allowToastClose : false,
     hideAfter : 5000,
     stack : 5,
+    loaderBg: '#fff',
     textAlign : 'left',
     position : 'bottom-left'
 });
@@ -16,8 +17,9 @@ $.toast({
     bgColor : '#4FC1E9',
     textColor : '#eee',
     allowToastClose : false,
-    hideAfter : 5000,
+    hideAfter : 10000,
     stack : 10,
+    loaderBg: '#fff',
     textAlign : 'left',
     position : 'bottom-left'
 });
