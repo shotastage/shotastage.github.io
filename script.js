b71501340b32f02170c2139f8998d7a27018a3b1
Shota Shimazu <hornet.live.mf@gmail.com>
2017-12-02 01:07:47 +0900

    update

--- script.js
+++
@@ -1,5 +0,0 @@
-
-
-// scrollreveal
-window.sr = ScrollReveal({ duration: 2000 });
-sr.reveal('.rv', 50);
--- utils/build.py
+++ utils/build.py
@@ -31,8 +31,8 @@ def Log(string, withError = False, withNotify = False):
 def preparation():
 
     # Check deploy gate dir.
-    if not os.path.isdir("./new/dist"):
-        os.mkdir("./new/dist")
+    if not os.path.isdir("./src/dist"):
+        os.mkdir("./src/dist")
         Log("Created distribution directory.")
 
     if not os.path.isdir("./node_modules"):
@@ -49,8 +49,8 @@ def preparation():
 # Cleaner
 def clean():
     Log("Cleaning source tree.")
-    if os.path.isdir("./new/dist"):
-        shutil.rmtree("./new/dist/")
+    if os.path.isdir("./src/dist"):
+        shutil.rmtree("./src/dist/")
 
 
 class Compiler():
@@ -58,14 +58,14 @@ class Compiler():
     def compile_pug(self):
         target_pages = []
 
-        for pug_files in os.listdir(os.getcwd() + "/new"):
+        for pug_files in os.listdir(os.getcwd() + "/src"):
             if ".pug" in pug_files:
                 target_pages.append(pug_files)
 
         Log("Compiling pug...")
         for page in target_pages:
             try:
-                output = subprocess.check_output(["./node_modules/.bin/pug", os.getcwd() + "/new/" + page, "--out", "./new/dist/"])
+                output = subprocess.check_output(["./node_modules/.bin/pug", os.getcwd() + "/src/" + page, "--out", "./src/dist/"])
             except:
                 Log("Failed to compile Pug!", withError = True)
                 Log("You may not install pug and pug-cli?", withError = True)
@@ -76,7 +76,7 @@ class Compiler():
     def compile_sass(self):
         Log("Compiling sass...")
         try:
-            output = subprocess.check_output(["./node_modules/.bin/node-sass", "--output-style", "compressed", "./new/styles/site.scss", "./new/dist/site.css"])
+            output = subprocess.check_output(["./node_modules/.bin/node-sass", "--output-style", "compressed", "./src/styles/site.scss", "./src/dist/site.css"])
         except:
             Log("Failed to compile Sass!", withError = True)
             Log("You may not install node-sass?", withError = True)
@@ -92,14 +92,14 @@ class Compiler():
         # JavaScript Libraries
         Log("Reading JavaScript libraries...")
         try:
-            libraries = os.listdir("./new/scripts/libs")
+            libraries = os.listdir("./src/scripts/libs")
         except:
             Log("Failed to get JavaScripts!", withError = True)
 
         Log("Generating minified libraries...")
         for lib in libraries:
             if ".js" in lib:
-                out = subprocess.check_output(["./node_modules/.bin/javascript-obfuscator", "./new/scripts/libs/" + lib , "--output", "tmp.js"])
+                out = subprocess.check_output(["./node_modules/.bin/javascript-obfuscator", "./src/scripts/libs/" + lib , "--output", "tmp.js"])
                 with open("tmp.js", "r") as f:
                     script_string += f.read()
                     os.remove("tmp.js")
@@ -108,30 +108,30 @@ class Compiler():
         # Site Script
         Log("Reading site scripts...")
         try:
-            scripts = os.listdir("./new/scripts/")
+            scripts = os.listdir("./src/scripts/")
         except:
             Log("Failed to get JavaScripts!", withError = True)
 
         Log("Generating minified scripts...")
         for js in scripts:
             if ".js" in js:
-                out = subprocess.check_output(["./node_modules/.bin/javascript-obfuscator", "./new/scripts/" + js , "--selfDefending", "false", "--output", "tmp.js"])
+                out = subprocess.check_output(["./node_modules/.bin/javascript-obfuscator", "./src/scripts/" + js , "--selfDefending", "false", "--output", "tmp.js"])
                 with open("tmp.js", "r") as f:
                     script_string += f.read()
                     os.remove("tmp.js")
 
 
-        os.mkdir("./new/dist/script/")
-        with open("./new/dist/script/site.js", "w") as write_target:
+        os.mkdir("./src/dist/script/")
+        with open("./src/dist/script/site.js", "w") as write_target:
             write_target.write(script_string)
 
-        shutil.copy2("./new/dist/script/site.js", "./new/dist/site.js")
+        shutil.copy2("./src/dist/script/site.js", "./src/dist/site.js")
 
 
     def create_assets(self):   
         Log("Creating assets pack...")
         try:
-            dir_util.copy_tree('./new/assets/', './new/dist/assets/')
+            dir_util.copy_tree('./src/assets/', './src/dist/assets/')
         except:
             Log("Failed to copy assets directory. ", withError = True)
 
@@ -143,7 +143,7 @@ class Compiler():
 
 
 class DebugUtils():
-    def open_in_chrome(self, URL = os.curdir + "/new/dist/index.html"):
+    def open_in_chrome(self, URL = os.curdir + "/src/dist/index.html"):
         Log("Opening in Chrome...")
 
         if platform.system() == "Darwin":
@@ -166,11 +166,11 @@ class DebugUtils():
 
     def open_in_safari(self):
         Log("Opening in Safari...")
-        subprocess.call(["open", "-a", "Safari", os.curdir + "/new/dist/index.html"])
+        subprocess.call(["open", "-a", "Safari", os.curdir + "/src/dist/index.html"])
 
     def open_in_firefox(self):
         Log("Opening in Firefox...")
-        subprocess.call(["open", "-a", "Firefox", os.curdir + "/new/dist/index.html"])
+        subprocess.call(["open", "-a", "Firefox", os.curdir + "/src/dist/index.html"])
 
     def watch_src(self):
         try:
