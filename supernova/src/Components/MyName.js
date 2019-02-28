b85571c11fcf868ae6ade75a3a8c67c2b7127ca2
Shota Shimazu <hornet.live.mf@gmail.com>
2019-02-28 17:52:58 +0900

    merge stylesheet into js

--- supernova/src/Components/MyName.js
+++ supernova/src/Components/MyName.js
@@ -1,15 +1,31 @@
 import React from 'react';
-import './MyName.scss';
+import styled from 'styled-components';
+
+
+const NameHeaderBackground = styled.div`
+  background: #9AE0FF;
+`;
+
+const NameHeader = styled.h1`
+  font-size: 10rem;
+  font-family: 'Roboto', sans-serif;
+  font-weight: 900;
+  color: #FF4141;
+  margin: 20px;
+
+  @media screen and (max-width: 480px) {
+    font-size: 5rem;
+  }
+`;
 
 
 export class MyName extends React.Component {
 
-
   render() {
     return (
-        <div className="name_header_bg">
-            <h1 className="name_header">Shota Shimazu</h1>
-        </div>
+        <NameHeaderBackground>
+            <NameHeader>Shota Shimazu</NameHeader>
+        </NameHeaderBackground>
     );
   }
 }
