50825e910f0516895931ffbaa41c08fe755cd27d
Shota Shimazu <shotastage@users.noreply.github.com>
2019-03-04 17:37:47 +0900

    Update precache-manifest.7132965c27f8e8f2e3fcc49dd8ec084f.js

--- supernova/current/precache-manifest.7132965c27f8e8f2e3fcc49dd8ec084f.js
+++ supernova/current/precache-manifest.7132965c27f8e8f2e3fcc49dd8ec084f.js
@@ -1,34 +1,34 @@
 self.__precacheManifest = [
   {
     "revision": "f4edaa0f9ee7be3c8c05",
-    "url": "/static/css/main.cc3d76f6.chunk.css"
+    "url": "./static/css/main.cc3d76f6.chunk.css"
   },
   {
     "revision": "f4edaa0f9ee7be3c8c05",
-    "url": "/static/js/main.f4edaa0f.chunk.js"
+    "url": "./static/js/main.f4edaa0f.chunk.js"
   },
   {
     "revision": "fdfcfda2d9b1bf31db52",
-    "url": "/static/js/runtime~main.fdfcfda2.js"
+    "url": "./static/js/runtime~main.fdfcfda2.js"
   },
   {
     "revision": "dc1d03fc64a5171931a6",
-    "url": "/static/js/2.dc1d03fc.chunk.js"
+    "url": "./static/js/2.dc1d03fc.chunk.js"
   },
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "url": "./static/media/logo.5d5d9eef.svg"
   },
   {
     "revision": "f8096cb9a394ec90bff0d3919b8a50c3",
-    "url": "/static/media/pinna_writing.f8096cb9.png"
+    "url": "./static/media/pinna_writing.f8096cb9.png"
   },
   {
     "revision": "d672915e73a8ba866d122e1909c2b1f4",
-    "url": "/static/media/bg.d672915e.svg"
+    "url": "./static/media/bg.d672915e.svg"
   },
   {
     "revision": "77d000fa925f445491840d1d6a0cf024",
-    "url": "/index.html"
+    "url": "./index.html"
   }
 ];
