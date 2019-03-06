c5f468aff0171b46bc7d60acf6187399f53837fd
Shota Shimazu <hornet.live.mf@gmail.com>
2019-03-06 11:50:50 +0900

    rename

--- build.sh
+++ build.sh
@@ -1,25 +1,52 @@
 #!/usr/bin/env bash
 
-cd ./supernova/
-if [ -e ./build/ ]; then
+mv ./index.html ./tmp.html
+
+
+clean() {
+    rm -rf ./static/
+    rm ./manifest.json
+    rm ./asset-manifest.json
+    rm ./favicon.ico
+    rm ./new.html
+    rm ./service-worker.js
+
+    for filepath in `ls | grep "package-manifest"`; do
+        rm $filepath
+    done
+}
+
+build() {
+    cd ./supernova/
+    if [ -e ./build/ ]; then
+        rm -rf ./build/
+    fi
+
+    yarn build
+    mv ./build/ ../build/
+    cd ..
+}
+
+
+deploy() {
+    cd ./build/
+    for filepath in `ls $PWD`; do
+        if [ -f $filepath ]; then
+            mv $filepath ../$filepath
+        fi
+
+        if [ -d $filepath ]; then
+            mv $filepath ../$filepath/
+        fi
+
+    done
+    cd ..
     rm -rf ./build/
-fi
+}
 
-yarn build
-mv ./build/ ../build/
-cd ..
+clean
+build
+deploy
 
-
-cd ./build/
-for filepath in `ls $PWD`; do
-    if [ -f $filepath ]; then
-        mv $filepath ../$filepath
-    fi
-
-    if [ -d $filepath ]; then
-        mv $filepath ../$filepath/
-    fi
-
-done
-cd ..
-rm -rf ./build/
+mv ./index.html ./new.html
+mv ./tmp.html ./index.html
