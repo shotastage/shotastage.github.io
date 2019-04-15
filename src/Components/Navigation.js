fde409c0d3a529fba7546aa8605fae402f1f191f
Shota Shimazu <hornet.live.mf@gmail.com>
2019-04-15 22:37:43 +0900

    make navigation letters bold

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import {
     Navbar, Nav, NavDropdown
 } from 'react-bootstrap';
@@ -7,12 +8,35 @@ import './Navigation.scss';
 
 
 
+
+const SHNavbar = styled(Navbar)`
+    background: white !important;
+    font-size: 1.5rem;
+    font-weight: bold;
+    color: #38393a;
+    height: 4.5rem;
+
+    @media (prefers-color-scheme: dark) {
+        background: #1A1A1A !important;
+    }
+`;
+
+const SHNavLink = styled(Nav.Link)`
+    color: #38393a !important;
+
+    @media (prefers-color-scheme: dark) {
+        color: white !important;
+    }
+`;
+
+
+
 export class Navigation extends React.Component {
 
   render() {
 
     return (
-        <Navbar bg="light" expand="lg">
+        <SHNavbar bg="light" expand="lg">
             <Navbar.Brand href="/">
                 <img
                     alt=""
@@ -25,8 +49,8 @@ export class Navigation extends React.Component {
             <Navbar.Toggle className="Navbar_Toggle" aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#link">SNS Link</Nav.Link>
+                    <SHNavLink href="#home">Home</SHNavLink>
+                    <SHNavLink href="#link">SNS Link</SHNavLink>
                     <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -36,7 +60,7 @@ export class Navigation extends React.Component {
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
-        </Navbar>
+        </SHNavbar>
     );
   }
 }
