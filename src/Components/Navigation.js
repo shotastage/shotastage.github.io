3c7c4607a1a509bf8e9ab3af48306153bbec0f7b
Shota Shimazu <hornet.live.mf@gmail.com>
2019-07-26 20:12:35 +0900

    remove dropdown

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import {
-    Navbar, Nav, NavDropdown
+    Navbar, Nav
 } from 'react-bootstrap';
 import logo from '../images/brand.svg';
 import './Navigation.scss';
@@ -51,13 +51,6 @@ export class Navigation extends React.Component {
                 <Nav className="mr-auto">
                     <SHNavLink href="#home">Home</SHNavLink>
                     <SHNavLink href="#link">SNS Link</SHNavLink>
-                    <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                    </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </SHNavbar>
